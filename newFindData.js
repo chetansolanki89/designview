88e25448e68c817c05cf4da3c8befeea22dba034
Chetan R. Solanki <chetansolanki.89@gmail.com>
2021-11-22 13:20:30 +0530

    Create newFindData.js

---
+++ newFindData.js
@@ -0,0 +1,8 @@
+const findData = (str, subStr) => {
+  const arrStrr = str.split(" ");
+  return arrStrr.indexOf(subStr) !== -1;
+};
+
+console.log(findData("I belong to Pune city.", "Pune"));
+console.log(findData("I am Indian and from Nanded", "Nanded"));
+console.log(findData("I am Indian and from Mumbai", "Nanded"));
